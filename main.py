49ab8d5ee447d695afcf5345233a41d93d361ecc
Tiago Oliveira <86539643+tiagoliveira555@users.noreply.github.com>
2022-10-20 01:18:53 +0000

    Meus primeiros códigos em Python.

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
-name = input('Digite seu nome: ')
+# Meu primeiro projeto Python!!!
 
-print(name)
+print('Hello World!\n')
+
+nome = 'Tiago'
+idade = 36
+
+print(f'Meu nome é {nome} e tenho {idade} anos de idade')
