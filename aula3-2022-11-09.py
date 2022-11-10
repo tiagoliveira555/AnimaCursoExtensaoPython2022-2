f9146b1cec3ca0d85a18b14700f0faf9fe2bfc90
Tiago Oliveira <86539643+tiagoliveira555@users.noreply.github.com>
2022-11-10 01:02:28 +0000

    Aula03

---
+++ aula3-2022-11-09.py
@@ -0,0 +1,22 @@
+print('aula3-2022-11-09.py')
+print()
+
+from time import sleep
+
+contador = 1
+
+while (contador <= 10):
+  print(contador)
+  contador += 1
+  sleep(1)
+print()
+
+print('Calcular imposto:')
+def calcular_importo(valor):
+  imposto = preco_produto * 0.05
+  return imposto
+
+preco_produto = 299
+
+imposto = calcular_importo(preco_produto)
+print(imposto)
