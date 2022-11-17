5d4ae7031a1bdad91be389f9778569d486ff3d99
Tiago Oliveira <86539643+tiagoliveira555@users.noreply.github.com>
2022-11-17 12:12:17 +0000

    acrescentado desafio proposto em aula

--- aula4_2022_11_16e.py
+++ aula4_2022_11_16e.py
@@ -13,8 +13,23 @@ tipo = 'Herói(na)'
 if (tipo_numerico == '2'):
   tipo = 'Vilã(o)'
 
+sql = "SELECT grupo_id, nome FROM grupos"
+
+cur.execute(sql)
+
+grupos = cur.fetchall()
+
+print('Grupos:')
+for grupo in grupos:
+  print(f'[ {grupo[0]} ] - {grupo[1]}')
+
+grupo_id = int(input('Sua opção: '))
+
 sql = f"INSERT INTO pessoas (pessoa_id, nome, nome_civil, tipo) VALUES ({pessoa_id}, '{nome}', '{nome_civil}', '{tipo}')"
 
 cur.execute(sql)
+
+sql = f"INSERT INTO pessoas_grupos (pessoa_id, grupo_id) VALUES ({pessoa_id}, {grupo_id})"
+
 con.commit()
 con.close()
